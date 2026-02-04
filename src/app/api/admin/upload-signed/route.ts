a8081e8a21c7e2f72b38210d031a7177d7a3a562
simonhayes51 <simonhayes_@hotmail.com>
2026-02-04 10:57:09 +0000

    Update route.ts

--- src/app/api/admin/upload-signed/route.ts
+++ src/app/api/admin/upload-signed/route.ts
@@ -34,7 +34,6 @@ export async function POST(request: Request) {
     headers: {
       host,
       "content-type": contentTypeValue,
-      "x-amz-acl": "public-read",
     },
     expires: 3600,
   })
